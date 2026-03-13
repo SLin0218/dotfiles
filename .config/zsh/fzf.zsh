cede839bad1e148e51d3a8d591d3673b3982b979
DengShilin <dengsl.dev@gmail.com>
2026-03-13 14:35:08 +0800

    sync

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -1,15 +1,11 @@
 export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build}"
 
-export CATPPUCCIN_MOCHA="--color=bg+:#1e1e2e,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
---color=marker:#f5e0dc,fg+:#a6e3a1,prompt:#cba6f7,hl+:#f38ba8"
-
-export CATPPUCCIN_LATTE="--color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39
---color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78
---color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39
---color=selected-bg:#bcc0cc"
-
-export DRACULA='--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+export THEME=" \
+--color=bg+:#363A4F,bg:#24273A,spinner:#F4DBD6,hl:#ED8796 \
+--color=fg:#CAD3F5,header:#ED8796,info:#C6A0F6,pointer:#F4DBD6 \
+--color=marker:#B7BDF8,fg+:#CAD3F5,prompt:#C6A0F6,hl+:#ED8796 \
+--color=selected-bg:#494D64 \
+--color=border:#6E738D,label:#CAD3F5"
 
 export FZF_DEFAULT_OPTS="
 --layout=reverse
@@ -17,7 +13,7 @@ export FZF_DEFAULT_OPTS="
 --border
 --no-separator
 --bind 'alt-y:execute(echo -n {} | xclip -selection clipboard)'
-${DRACULA}
+${THEME}
 "
 
 export FORGIT_BLAME_FZF_OPTS="--preview-window='right:60%'"
@@ -28,14 +24,8 @@ export FORGIT_FZF_DEFAULT_OPTS="
 --no-separator
 --height '100%'
 --preview-window=right,70%
-${DRACULA}
+${THEME}
 "
-
-# export FZF_TMUX_OPTS="-p50%,50%
-# --color=bg+:#1e1e2e,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
-# --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
-# --color=marker:#f5e0dc,fg+:#a6e3a1,prompt:#cba6f7,hl+:#f38ba8,border:#cba6f7"
-
 
 export FZF_CTRL_R_OPTS=$FORGIT_FZF_DEFAULT_OPTS
 export FZF_CTRL_T_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build}"
@@ -47,4 +37,10 @@ zstyle ':fzf-tab:*' default-color $'\033[34m'
 zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
 zstyle ':fzf-tab:*' switch-group ',' '.'
 zstyle ':fzf-tab:*' fzf-pad 4
-zstyle ':fzf-tab:*' fzf-flags --no-separator --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4
+zstyle ':fzf-tab:*' fzf-flags --no-separator \
+--color=bg+:#363A4F,bg:#24273A,spinner:#F4DBD6,hl:#ED8796 \
+--color=fg:#CAD3F5,header:#ED8796,info:#C6A0F6,pointer:#F4DBD6 \
+--color=marker:#B7BDF8,fg+:#CAD3F5,prompt:#C6A0F6,hl+:#ED8796 \
+--color=selected-bg:#494D64 \
+--color=border:#6E738D,label:#CAD3F5
+
